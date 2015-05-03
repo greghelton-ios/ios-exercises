c13b36373fe0a211d58cd6034c6e08ed2e35053b
greghelton-ios <greg.helton.tx@gmail.com>
2015-05-03 17:45:28 -0500

    Completed Loops/AwesomeCounter and AwesomeCounterTests

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -14,12 +14,18 @@
     /* WORK HERE */
     NSMutableString *concatString = [[NSMutableString alloc] init];
     
+    /*
+         number should be the lower of the two numbers so make the switch if necessary
+     */
     if (number > otherNumber) {
         NSInteger temp = number;
         number = otherNumber;
         otherNumber = temp; 
     }
 
+    /*
+          from lower number to higher number append so that
+     */
     for (NSInteger x = number; x <= otherNumber; x++)
     {
         [concatString appendFormat: @"%li", (long)x];
